0990b21b4365b00c20a7df0c522b640e2fcf8a20
DocCTF <ernesthaban679@gmail.com>
2021-05-09 16:44:15 +0200

    update

---
+++ HWinfo.py
@@ -0,0 +1,6 @@
+import psutil
+from colorama import init, Fore, Back
+from Main import Main 
+init(autoreset=True)
+main = Main()
+main.test()
---
+++ Main.py
@@ -0,0 +1,26 @@
+import psutil
+from colorama import init, Fore, Back
+class Main:
+    def __init__(self):
+        self.headNadpis = "HWInfo"
+        self.headText = 25
+        self.lineLen = 100
+        self.bcgColor = Back.LIGHTBLACK_EX
+        self.txtColor = Fore.LIGHTWHITE_EX
+        self.riadokFarba = Back.LIGHTCYAN_EX
+    def center(self, text):
+        textlen = len(text)
+        if textlen %2 != 0:
+            text += " "
+        zoradenie = (self.lineLen - textlen) // 2
+        return self.riadokFarba + " " * zoradenie + text + " " * zoradenie + "‎"
+    def line(self):
+        return self.riadokFarba + " " * self.lineLen + "‎"
+    
+    def test(self):
+        print(self.line())
+        print(self.line())
+        print(self.center("HWInfo1"))
+        print(self.line())
+        print(self.center("Created by @ Esec Team 2021"))
+        print(self.line())
